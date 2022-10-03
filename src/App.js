f296932c80b774dd54a0176b68609874cd5a6595
Faisal K <faisalkkn9946@gmail.com>
2022-10-03 12:03:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import {useState} from 'react'
+import NavBar from "./Components/NavBar/NavBar";
+import Banner from "./Components/Banner/Banner";
+import RowPost from "./Components/RowPost/RowPost";
+import './App.css'
 
 function App() {
+  const [state, setState] = useState([])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Banner />
+      <RowPost />
+      <h1>Hello WOrld</h1>
+      {/* <button onClick={()=>{
+        axios.get('https://jsonplaceholder.typicode.com/posts').then((response)=>{
+          console.log(response.data);
+          setState(response.data)
+        })
+      }}>Click me</button>
+      {
+        state.map((obj, index)=>{
+          return(
+            <div>
+              <h1>{index + 1}</h1>
+              <h1>{obj.title}</h1>
+              <h4>{obj.body}</h4>
+
+            </div>
+          )
+        })
+      } */}
     </div>
   );
 }
